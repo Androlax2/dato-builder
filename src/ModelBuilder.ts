5c8b15f493f8f794f57c4ee721c560d0aaceecb4
Théo Benoit <39646949+Androlax2@users.noreply.github.com>
2025-07-22 15:00:11 -0400

    fix: update ModelBuilderBody type to include fields from SimpleSchemaTypes and add optional api_key

--- src/ModelBuilder.ts
+++ src/ModelBuilder.ts
@@ -1,12 +1,41 @@
+import type * as SimpleSchemaTypes from "@datocms/cma-client/src/generated/SimpleSchemaTypes";
 import ItemTypeBuilder, {
-  type ItemTypeBuilderBody,
   type ItemTypeBuilderType,
 } from "./ItemTypeBuilder.js";
 import type { ResolvedDatoBuilderConfig } from "./types/DatoBuilderConfig.js";
 
+type ModelBuilderBody = Pick<
+  SimpleSchemaTypes.ItemTypeCreateSchema,
+  | "hint"
+  | "tree"
+  | "sortable"
+  | "singleton"
+  | "draft_mode_active"
+  | "draft_saving_active"
+  | "all_locales_required"
+  | "workflow"
+  | "inverse_relationships_enabled"
+  | "collection_appearance"
+  | "ordering_meta"
+  | "ordering_direction"
+  | "ordering_field"
+  | "presentation_title_field"
+  | "presentation_image_field"
+  | "title_field"
+  | "image_preview_field"
+  | "excerpt_field"
+> & {
+  /**
+   * API key of the block.
+   *
+   * If not provided, it will be generated from the name.
+   */
+  api_key?: string;
+};
+
 type ModelBuilderOptions = {
   name: string;
-  body?: Omit<ItemTypeBuilderBody, "name">;
+  body?: ModelBuilderBody;
   config: ResolvedDatoBuilderConfig;
 };
 
